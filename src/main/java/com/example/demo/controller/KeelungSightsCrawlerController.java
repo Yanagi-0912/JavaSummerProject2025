1294df90b3c7b0c3ca3b033461417ee217fd8a33
Yanagi-0912 <930912jeffery@gmail.com>
2025-07-06 20:06:22 +0800

    改成http://127.0.0.1:8080/SightAPI?zone=七堵

--- src/main/java/com/example/demo/controller/KeelungSightsCrawlerController.java
+++ src/main/java/com/example/demo/controller/KeelungSightsCrawlerController.java
@@ -3,15 +3,17 @@ package com.example.demo.controller;
 import com.example.demo.model.KeelungSightsCrawler;
 import com.example.demo.model.Sight;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @RestController
 public class KeelungSightsCrawlerController {
-    @GetMapping("sight/{zone}")
-    public ResponseEntity<Sight[]> getSights(@PathVariable("zone") String zone) {
+    @GetMapping("/SightAPI")
+    public ResponseEntity<Sight[]> getSights(
+            @RequestParam(value = "zone", required = false, defaultValue = "") String zone
+    ) {
         KeelungSightsCrawler crawler = new KeelungSightsCrawler();
         Sight[] sights = crawler.getItems(zone);
         if (sights == null || sights.length == 0) {
@@ -21,4 +23,4 @@ public class KeelungSightsCrawlerController {
     }
 }
 
-//http://localhost:8080/sight/七堵
+//http://127.0.0.1:8080/SightAPI?zone=七堵
