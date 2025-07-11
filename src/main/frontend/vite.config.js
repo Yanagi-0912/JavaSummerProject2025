19a21059ecda5731b193db005d631a4090280b73
Yanagi-0912 <930912jeffery@gmail.com>
2025-07-12 04:56:10 +0800

    構建前端

--- src/main/frontend/vite.config.js
+++ src/main/frontend/vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()
-  ],
+  plugins: [react()],
 
   server: {
     port: 3000
+  },
+
+  build: {
+    outDir: '../resources/static',
+    emptyOutDir: true
   }
 })
